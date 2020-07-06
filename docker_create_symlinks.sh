6449ac8186dc2ca41e4b3573f05a794d0a4d7353
Trent Robbins <robbintt@gmail.com>
2020-07-05 19:44:14 -0700

    destination wasn't used before whoops

--- docker_create_symlinks.sh
+++ docker_create_symlinks.sh
@@ -8,9 +8,9 @@ destination=../lich/scripts
 # NOTE: this requires gnutools on macos homebrew
 
 # Delete all symlinks from lich/scripts
-gfind $1 -maxdepth 1 -type l -exec rm -f {} \;
+gfind $destination -maxdepth 1 -type l -exec rm -f {} \;
 
 # Link all the things
-gln -rs *.lic $1
-gln -rs /profiles/ $1
-gln -rs /data/ $1
+gln -rs *.lic $destination
+gln -rs profiles $destination/profiles
+gln -rs data $destination/data
